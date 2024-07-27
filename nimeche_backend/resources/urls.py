d33ac7f4637c34e6a1b21a2096064134eed476e1
oliverTwist2 <oliverikegah@gmail.com>
2024-07-27 22:01:27 +0100

    added routes for blog and materials

--- nimeche_backend/resources/urls.py
+++ nimeche_backend/resources/urls.py
@@ -1,2 +1,13 @@
 from django.urls import path
+from .views import BlogView, BlogDetailView, MaterialListView, MaterialDetailView
+from .views import ResourceListView, ResourceDetailView
+
+urlpatterns = [
+    path('blogs/', BlogView.as_view(), name='blogs'),
+    path('blogs/<int:pk>/', BlogDetailView.as_view(), name='blog'),
+    path('materials/', MaterialListView.as_view(), name='materials'),
+    path('materials/<int:pk>/', MaterialDetailView.as_view(), name='material'),
+    path('resources/', ResourceListView.as_view(), name='resources'),
+    path('resources/<int:pk>/', ResourceDetailView.as_view(), name='resource'),
+]
 
